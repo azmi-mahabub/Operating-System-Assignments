9d989c738f16bc113c7765220c702c76e8c07cd8
azmi-mahabub <154908471+azmi-mahabub@users.noreply.github.com>
2025-09-20 13:51:46 +0600

    Add files via upload

---
+++ Disk_CSCAN.py
@@ -0,0 +1,38 @@
+req_s = input("Enter numbers: ")
+req_s = list(map(int, req_s.split()))
+head = int(input("Enter the value of head: "))
+direction = input("Enter the direction: ")
+direction = direction.lower()
+
+sum = 0
+
+req_s.sort(key=lambda x: x)
+
+if direction == "left":
+  for i in range(len(req_s)):
+    if req_s[i] == head:
+      for j in range(i, 0, -1):
+        sum += abs(req_s[j] - req_s[j-1])
+        print(sum)
+
+      sum+=abs(req_s[0]-req_s[-1])
+      print(sum)
+
+      for j in range(len(req_s)-1, i+1, -1):
+        sum += abs(req_s[j] - req_s[j-1])
+        print(sum)
+
+elif direction == "right":
+  for i in range(len(req_s)):
+    if req_s[i] == head:
+      for j in range(i, len(req_s)-1):
+        sum += abs(req_s[j] - req_s[j+1])
+        print(sum)
+
+      sum+=abs(req_s[-1]-req_s[0])
+      print(sum)
+
+      for j in range(0, i-1):
+        sum += abs(req_s[j] - req_s[j+1])
+        print(sum)
+print("Total Seek Time = ", sum)
